85dc3254815e0bbb161e53a500db9e1bd0faf0eb
Marc <marc.lallias@icloud.com>
2020-10-02 07:01:17 +0900

    Initial Commit
    hello world

---
+++ API_JSON_abstractedTests/API_JSON_abstractedTests.swift
@@ -0,0 +1,34 @@
+//
+//  API_JSON_abstractedTests.swift
+//  API_JSON_abstractedTests
+//
+//  Created by marc lallias on 02/10/2020.
+//  Copyright © 2020 marc lallias. All rights reserved.
+//
+
+import XCTest
+@testable import API_JSON_abstracted
+
+class API_JSON_abstractedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
