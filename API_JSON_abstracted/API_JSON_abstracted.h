c3c5289555e3a6312877443897ccbff257c473e2
Marc <marc.lallias@icloud.com>
2020-10-02 07:01:17 +0900

    Initial Commit

---
+++ API_JSON_abstracted/API_JSON_abstracted.h
@@ -0,0 +1,19 @@
+//
+//  API_JSON_abstracted.h
+//  API_JSON_abstracted
+//
+//  Created by marc lallias on 02/10/2020.
+//  Copyright © 2020 marc lallias. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for API_JSON_abstracted.
+FOUNDATION_EXPORT double API_JSON_abstractedVersionNumber;
+
+//! Project version string for API_JSON_abstracted.
+FOUNDATION_EXPORT const unsigned char API_JSON_abstractedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <API_JSON_abstracted/PublicHeader.h>
+
+
